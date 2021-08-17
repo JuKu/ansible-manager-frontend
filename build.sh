47e0492996f1af9eb8c9de6a7f9dfa7c36f6f221
JuKu <kuenzel.justin@t-online.de>
2021-08-17 22:12:02 +0200

    Add ionic

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cd /opt/anman/anman-frontend
+
+# Git Pull
+git fetch --all
+git reset --hard origin/master
+git pull
+
+chmod 755 build.sh
+
+echo "Stop old screen session"
+../bin/stopScreen.sh
+
+echo "Start build"
+mvn clean install
+
+echo "copy jars and configuration"
+cp -rf mmo-management-main/target/mmo-management-main-1.0.0-SNAPSHOT.jar ../bin/app.jar
+#cp -rf mmo-management-main/target/lib ../bin/lib
+
+# Copy configuration
+cp -rf ../config/application.properties ../bin/application.properties
+
+echo "Start new screen session"
+
+cd /opt/mmo-management-console/bin
+./startScreen.sh
