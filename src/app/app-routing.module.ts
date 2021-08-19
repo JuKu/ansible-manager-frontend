909991c84da5f13fef02fbbbb092254dce4e1fbe
JuKu <kuenzel.justin@t-online.de>
2021-08-19 23:59:56 +0200

    Add new user module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'folder/inbox',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then(m => m.FolderPageModule)
+  },
+  {
+    path: 'user/',
+    pathMatch: 'prefix',
+    loadChildren: () => import('./features/user/user.module').then(m => m.UserModule)
   }
 ];
 
---
+++ src/app/features/user/user.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {FormsModule} from '@angular/forms';
+
+import {IonicModule} from '@ionic/angular';
+import {UserRoutingModule} from './user-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    UserRoutingModule
+  ]
+})
+export class UserModule { }
