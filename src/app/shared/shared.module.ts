2c7f7f7dada004382284e12d45c8557bbcc17ee0
JuKu <kuenzel.justin@t-online.de>
2021-08-20 23:17:50 +0200

    Add SharedModule

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {Error404Component} from './errors/error404/error404.component';
+
+@NgModule({
+  declarations: [
+    Error404Component
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    Error404Component
+  ]
+})
+export class SharedModule {
+  static forRoot() {
+    return {
+      ngModule: SharedModule,
+      providers: []
+    };
+  }
+}
