d82c9be6819367b2f813709215f26878cac0a11f
JuKu <kuenzel.justin@t-online.de>
2021-08-26 03:53:36 +0200

    Add some unit tests

--- src/app/core/rest/rest-api.service.spec.ts
+++ src/app/core/rest/rest-api.service.spec.ts
@@ -21,4 +21,25 @@ describe('RestAPIService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return a base url', () => {
+    expect(service.getBaseURL).toBeTruthy();
+    expect(service.getBaseURL()).toBe('http://127.0.0.1:8080/');
+  });
+
+  it('should return http options', () => {
+    expect(service.getHttpOptions()).toBeTruthy();
+
+    // check, that http header contains content-type and access-control
+    expect(service.getHttpOptions().headers.get('Content-Type')).toBeTruthy();
+    expect(service.getHttpOptions().headers.get('Content-Type')).toBe('application/json');
+
+    expect(service.getHttpOptions().headers.get('Access-Control-Allow-Origin')).toBeTruthy();
+    expect(service.getHttpOptions().headers.get('Access-Control-Allow-Origin')).toBe('*');
+  });
+
+  it('should return resource url', () => {
+    expect(service.getResourceURL('test')).toBeTruthy();
+    expect(service.getResourceURL('test')).toBe('http://127.0.0.1:8080/test');
+  });
 });
