6e9e5559feb1e3ab59204ba167e751fbbf42f5ae
JuKu <kuenzel.justin@t-online.de>
2022-01-03 20:08:52 +0100

    Add environmental variables for version title

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   backendApiUrl: '${BACKEND_API_URL}',
-  defaultLanguage: '${DEFAULT_LANGUAGE}'
+  defaultLanguage: '${DEFAULT_LANGUAGE}',
+  versionName: '0.0.1 Pre-Alpha'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   backendApiUrl: 'http://127.0.0.1:8080/',
-  defaultLanguage: 'de'
+  defaultLanguage: 'de',
+  versionName: '0.0.1 Pre-Alpha'
 };
 
 /*
